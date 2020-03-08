fe03ecf882266311b532e8668f5898d35cafe281
Lazare-C <lazarela17@gmail.com>
2020-03-08 16:57:51 +0100

    01

---
+++ install-android.sh
@@ -0,0 +1,3 @@
+./android/gradlew assembledebug
+adb.exe install /home/titi/Developer/ang03/android/app/build/outputs/apk/debug/app-debug.apk
+
--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
+./gradlew assembledebug
 adb.exe install /home/titi/Developer/ang03/android/app/build/outputs/apk/debug/app-debug.apk
 
